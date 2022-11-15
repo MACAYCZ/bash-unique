4ab7d4e0f98caa20def774e81ee7ab5801cbc4e7
Petr Holčák <44314775+MACAYCZ@users.noreply.github.com>
2022-11-15 21:00:33 +0100

    Version 2.0

--- src/unique.sh
+++ src/unique.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-declare -r _unique_prefix="_"
-declare -i _unique_current=0
+declare -r _unique_prefix_='_'
+declare -i _unique_current_=0
 
-unique_tst() {
+unique_test() {
 	if typeset -p $1 2> /dev/null | grep -q '^'; then echo 1; else echo 0; fi
 }
 
+unique_type() {
+	local _unique_info_=$(declare -p $1)
+	echo ${_unique_info_:9:1}
+}
+
 unique_new() {
-    declare -g$2 $_unique_prefix$_unique_current
-    local -n _out=$1; _out=$_unique_prefix$_unique_current
-	while [[ $(unique_tst $_unique_prefix$_unique_current) == 1 ]]; do
-		_unique_current=$(( _unique_current + 1 ))
+	local _unique_options_=$2; if [[ $_unique_options_ == '--' ]]; then _unique_options_='  '; fi
+	declare -g${_unique_options_:1} $_unique_prefix_$_unique_current_
+	local -n _unique_out_=$1; _unique_out_=$_unique_prefix_$_unique_current_
+	while [[ $(unique_test $_unique_prefix_$_unique_current_) == 1 ]]; do
+		_unique_current_=$(( $_unique_current_ + 1 ))
 	done
 }
 
 unique_del() {
-	local _name=$1
-	if [[ ${_name#$_unique_prefix} < $_unique_current ]]; then
-		_unique_current=${_name#$_unique_prefix}
+	local _unique_name_=$1
+	if [[ ${_unique_name_#$_unique_prefix_} < $_unique_current_ ]]; then
+		_unique_current_=${_unique_name_#$_unique_prefix_}
 	fi
 	unset $1
 }
